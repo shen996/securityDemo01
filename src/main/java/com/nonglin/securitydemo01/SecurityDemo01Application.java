91e70252cc6572a131528f80fd8954b3e3aa6279
shen <szl@xyafu.edu.cn>
2022-04-27 18:34:49 +0800

    Initial commit

---
+++ src/main/java/com/nonglin/securitydemo01/SecurityDemo01Application.java
@@ -0,0 +1,13 @@
+package com.nonglin.securitydemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemo01Application.class, args);
+    }
+
+}
